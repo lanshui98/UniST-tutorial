e6e7bd9e299b077371af1c05501fb55c6319ae21
Lan Shui <lshui@mdanderson.org>
2026-02-05 15:42:39 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,14 +31,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_logo = "__static/unist_logo.png"
-html_theme_options = {
-    "logo_only": True,
-}
-
-html_css_files = [
-    "__static/custom.css",
-]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
